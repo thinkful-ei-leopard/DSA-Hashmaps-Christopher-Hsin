574f20938270e231020a4468624f6753bb36be3f
Hsin Ling Hu <hsinkava11@gmail.com>
2020-04-06 09:38:12 -0700

    completed 1. Create a HashMap class

---
+++ HashMaps_drills.js
@@ -0,0 +1,31 @@
+const HashMap = require('./HashMaps');
+
+function main(){
+  let lotr = new HashMap;
+  lotr.MAX_LOAD_RATIO = 0.5;
+  lotr.SIZE_RATIO = 3; 
+  lotr.set("Hobbit", "Bilbo");
+  lotr.set("Hobbit", "Frodo");
+  lotr.set("Wizard", "Gandalf");
+  lotr.set("Human", "Aragorn");
+  lotr.set("Elf", "Legolas");
+  lotr.set("Maiar", "The Necromancer");
+  lotr.set("Maiar", "Sauron");
+  lotr.set("RingBearer", "Gollum");
+  lotr.set("LadyOfLight", "Galadriel");
+  lotr.set("HalfElven", "Arwen");
+  lotr.set("Ent", "Treebeard");
+  console.log(lotr.get("Maiar"));
+  console.log(lotr.get("Hobbit"));
+  
+  return lotr;
+}
+
+console.log(main());
+
+/*What are the values of Maiar and Hobbit that you have? Maiar:Sauron, Hobbit: Frodo
+Is there a discrepancy? only received one of each value, instread of two.*/
+
+//What is the capacity of your hash table after you have hashed all the above items? 
+// 8 (same like initialCapacity we set)
+
