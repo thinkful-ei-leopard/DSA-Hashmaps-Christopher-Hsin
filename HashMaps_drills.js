ad11fecb5204961b5ccb1aa3f3b5e76bcff8afe8
Hsin Ling Hu <hsinkava11@gmail.com>
2020-04-06 14:25:33 -0700

    complete 4. Remove duplicates

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -1,5 +1,7 @@
 const HashMap = require('./HashMaps');
 
+
+
 function main(){
   HashMap.MAX_LOAD_RATIO = 0.5;
   HashMap.SIZE_RATIO = 3; 
@@ -21,7 +23,7 @@ function main(){
   return lotr;
 }
 
-console.log(main())
+//console.log(main())
 
 /*What are the values of Maiar and Hobbit that you have? Maiar:Sauron, Hobbit: Frodo
 Is there a discrepancy? only received one of each value, instread of two.*/
@@ -32,3 +34,25 @@ Is there a discrepancy? only received one of each value, instread of two.*/
 //2. WhatDoesThisDo
 //console.log(map1.get(str1)); ---20 the value of str2 will replace str1
 //console.log(map2.get(str3)); ---10 the value of str4 will replace str3
+
+//drill 4
+function removeDuplicates(str) {
+  //keep char first time it's seen. remove if it repeats
+  let removeDup = new HashMap;
+  let newStr= ''
+  for(let i=0; i< str.length; i++){
+    try {
+      removeDup.get(str[i])
+    }
+    catch(e) {
+      removeDup.set(str[i], 1)
+      newStr += str[i]
+    }
+  }
+  
+  return newStr;
+}
+
+console.log(removeDuplicates('google all that you think can think of'));
+
+//drill 5
